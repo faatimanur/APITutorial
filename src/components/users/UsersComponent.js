a8365ba5a0fb6fbcef6512ad0da97dea6e863ee5
faatimanur <dev,fatimanoor@gmail.com>
2023-04-23 14:02:12 +0500

    select

--- src/components/users/UsersComponent.js
+++ src/components/users/UsersComponent.js
@@ -4,12 +4,24 @@ import {useState, useEffect } from 'react';
 
  function UsersComponent() {
   const [users, setUsers] = useState([]);
+  const [select, setSelect] = useState(10);
+  const getUsers=(select)=>{
+    console.log(select);
+    axios.get(`https://jsonplaceholder.typicode.com/users?_limit=${select}`).then(response=>{
+    setUsers(response.data)
+  })
+  }
+
+  const handleChange=(e)=>{
+    e.preventDefault();
+    setSelect(e.target.value);
+    getUsers(e.target.value);
+  }
 
   useEffect(()=>{
-    axios.get('https://jsonplaceholder.typicode.com/users').then(response=>{
-    setUsers(response.data)
-    })
-  },[]);
+    getUsers()
+  },[])
+
   return (
     <div className='container'>
       <div className="row">
@@ -38,6 +50,21 @@ import {useState, useEffect } from 'react';
               }
             </tbody>
           </table>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-4">
+        <select className='form-control' onChange={(e)=>handleChange(e)}>
+            <option value="2">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+        </select>
         </div>
       </div>
     </div>
